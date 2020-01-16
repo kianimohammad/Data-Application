cbde49d68c3e747e94fd0d0695b0389609f72fe3
Mohammad Kiani <kianimohammad@outlook.com>
2020-01-16 17:10:30 -0500

    Initial Commit

---
+++ Data Application/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Data Application
+//
+//  Created by Mohammad Kiani on 2020-01-16.
+//  Copyright © 2020 mohammadkiani. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
